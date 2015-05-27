931648de118b786d65a20af6f989830ce724aac6
Lilli Szafranski (Jaguar - Land Rover) <lszafran@jaguarlandrover.com>
2015-05-27 13:31:26 -0700

    [master] Added Jetbrains preferences folder from AndroidStudio on JLR Macbook

---
+++ JetbrainsPrefs/fileTemplates/internal/Class.java
@@ -0,0 +1,7 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+public class ${NAME} {
+    private final static String TAG = "${PROJECT_NAME}:${NAME}";
+
+}
