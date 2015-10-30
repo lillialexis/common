fff52ef32abac3b4361a1489227fa3ab771eb5e8
Lilli Szafranski <lillialexis@gmail.com,lilli@daoofdev.com>
2015-10-30 16:12:56 -0700

    [appcode_prefs_home] Adding what should be only the files I really want to track

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,15 @@
+#set( $MyName = "Lilli Szafranski" )
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * 
+ * Copyright (c) ${YEAR} Dao of Development.
+ * 
+ * All rights reserved.
+ * 
+ * ${FILE_NAME}
+ * ${PROJECT_NAME}
+ * 
+ * Created by $MyName on ${DATE}.
+ * 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
